4e2fee328b35de1f602406be82eeb8624304e4f4
yangyu <1504239148@qq.com>
2020-12-21 20:09:58 +0800

    master提交

--- yanglaoyuan/src/main/java/com/yanglaoyuan/service/ConsultServices.java
+++ yanglaoyuan/src/main/java/com/yanglaoyuan/service/ConsultServices.java
@@ -1,15 +1,15 @@
 package com.yanglaoyuan.service;
 
-import com.yanglaoyuan.model.Mapper.UserMapper;
+
 import com.yanglaoyuan.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConsultServices {
-    @Autowired
-    UserMapper um;
-    public User login(String phone,String upass){
-        return um.login(phone, upass);
-    }
+//    @Autowired
+//    UserMapper um;
+//    public User login(String phone,String upass){
+//        return um.login(phone, upass);
+//    }
 }
